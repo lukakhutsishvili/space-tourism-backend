57459f3cdc74823f0ebf28813f12ca09553f4b75
Luka Khutsishvili <luka.khutsishvili777@gmail.com>
2024-03-21 20:23:23 +0400

    mongo_url

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connect = () => {
   try {
-    const Url = "mongodb+srv://lukakhutsishvili777:barcelona@cluster0.d9lbycm.mongodb.net/space_tourism";
-    mongoose.connect(Url);
+    const Url = process.env.MONGO_URL;
+    mongoose.connect(Url || "");
   } catch (error) {
     console.log(error);
   }
